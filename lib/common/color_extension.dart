4e0bb9b442ec3f25cd4f5dcc90bbfedad73b5ce5
Sonod Sadman <sonodsadman@gmail.com>
2024-01-13 15:17:05 +0600

    Sonod

--- lib/common/color_extension.dart
+++ lib/common/color_extension.dart
@@ -5,5 +5,6 @@ class TColor {
   static Color get primaryLight => const Color(0xff5AFDFC7);
   static Color get text => const Color(0xff212121);
   static Color get subTitle => const Color(0xff52be88);
+  static Color get lightBlue => const Color(0xffADD8E6);
   static List<Color> get button => const [Color(0xff5ABD8C), Color(0xff00FF81)];
 }
